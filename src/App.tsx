6ceeb7a38c5cf2b3578948522f666ddde57b0c81
Quresh <qureshmohsin53@gmail.com>
2025-08-03 21:03:28 +0500

    temp page, tailwind version migration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import TempPage from './components/TempPage'
 
 
 function App() {
@@ -6,13 +7,7 @@ function App() {
 
   return (
     <>
-     <div className='h-screen overflow-hidden'>
-      <div className='flex items-center flex-row gap-2'>
-
-    
-      <h1 className='text-5xl'>Hqm Solutions</h1>
-       </div>
-     </div>
+     <TempPage/>
     
     </>
   )
