44fdab90294722e8447ab32fd183d235014eee9b
Quresh <qureshmohsin53@gmail.com>
2025-08-03 14:35:54 +0500

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+     <div className='h-screen overflow-hidden'>
+      <div className='flex items-center flex-row gap-2'>
+
+    
+      <h1 className='text-5xl'>Hqm Solutions</h1>
+       </div>
+     </div>
+    
+    </>
+  )
+}
+
+export default App
